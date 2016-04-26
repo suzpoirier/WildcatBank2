3565956bb383b3eaba569313336cac0237fb288c
suzpoirier <suz.poirier@gmail.com>
2016-04-26 10:36:30 -0400

    Initial commit

---
+++ app/src/main/java/com/wildcatbank2/ContactUs.java
@@ -0,0 +1,112 @@
+package com.wildcatbank2;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ContactUs.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ContactUs#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactUs extends DialogFragment {
+
+
+    private OnFragmentInteractionListener mListener;
+    private ViewGroup mContainer;
+
+    public ContactUs() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContactUs.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactUs newInstance(String param1, String param2) {
+        return new ContactUs();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+       // builder.setMessage(R.string.contact_us_title);
+        builder.setNeutralButton(R.string.close_contact, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setView(getLayout(LayoutInflater.from(getContext()), mContainer));
+        return builder.create();
+    }
+
+    private View getLayout(LayoutInflater inflater, ViewGroup container){
+        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return null;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+      //  if (context instanceof OnFragmentInteractionListener) {
+      //      mListener = (OnFragmentInteractionListener) context;
+     //   } else {
+     //       throw new RuntimeException(context.toString()
+     //               + " must implement OnFragmentInteractionListener");
+     //   }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/wildcatbank2/LocateActivity.java
@@ -0,0 +1,189 @@
+package com.wildcatbank2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.content.Intent;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Hashtable;
+
+public class LocateActivity extends Activity implements AccountTabs.OnFragmentInteractionListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleMap map;
+    private GoogleApiClient mGoogleApiClient;
+    private Location currentLocation;
+    private Marker marker;
+    private Hashtable bankMarkers;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate);
+
+
+    }
+
+    private void initializeMap() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+        if (map == null){
+            map = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            criteria.setCostAllowed(false);
+
+            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            String provider = lm.getBestProvider(criteria, false);
+
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+            try {
+                lm.requestLocationUpdates(provider, 1000, 100, (LocationListener) this);
+                this.onLocationChanged(lm.getLastKnownLocation(provider));
+            } catch (Exception e){
+
+            }
+
+        }
+        if (map != null) {
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+            map.getUiSettings().setRotateGesturesEnabled(true);
+            MapsInitializer.initialize(this);
+
+
+        }
+        if (mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addOnConnectionFailedListener(this)
+                    .addConnectionCallbacks(this)
+                    .build();
+        }
+
+
+
+    }
+    public void onResume () {
+        super.onResume();
+        try {
+            // Loading map
+            initializeMap();
+            mGoogleApiClient.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient!= null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+    @Override
+    public void onAccountTabFragmentInteraction(Uri uri) {
+
+    }
+    public void selectAddressButton(View view) {
+        TextView addressText = (TextView)findViewById(R.id.editText);
+        addressText.setEnabled(true);
+    }
+    public void selectLocationButton(View view) {
+        TextView addressText = (TextView)findViewById(R.id.editText);
+        addressText.setEnabled(false);
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = map.getMyLocation();
+        if (location != null){
+            Log.d("location", location.toString());
+        }
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+    public void onLocationChanged(Location location){
+         if ((currentLocation == null) || (currentLocation.getLatitude() != location.getLatitude() || currentLocation.getLongitude() != location.getLongitude()) ){
+            if (marker != null){
+                marker.remove();
+            }
+            marker = map.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Marker"));
+            currentLocation = location;
+            updateMapCamera();
+        }
+
+    }
+
+    /**
+     * update this to have pretend addresses for bank locations
+     */
+    private void updateMapCamera(){
+        if (marker != null) {
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(marker.getPosition());
+            LatLngBounds bounds = builder.build();
+            int padding = 250;
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+            map.animateCamera(cu);
+        }
+    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+}
+
